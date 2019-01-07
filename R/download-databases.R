bd1761061a0a52bf12a9199f774f813eb76c38e4
Renata Diaz <diaz.renatam@gmail.com>
2019-01-07 15:36:09 -0500

    Adding quaternary mammals database

--- R/download-databases.R
+++ R/download-databases.R
@@ -42,3 +42,42 @@ load_mcdb <- function() {
 
 }
 
+#' Download Body Mass of Late Quaternary Mammals
+#'
+#' Saves data tables from https://esajournals.onlinelibrary.wiley.com/doi/abs/10.1890/02-9003 to `analysis/data/raw_data`.
+#'
+#' @return null
+#'
+#' @export
+download_quaternarymammals <- function(){
+  ifelse(!dir.exists(file.path('analysis/data/raw_data', 'quaternarymammals')), dir.create(file.path('analysis/data/raw_data', 'quaternarymammals')), FALSE)
+
+  download.file('https://ndownloader.figshare.com/files/5593340',
+                'analysis/data/raw_data/quaternarymammals/quaternarymammals_abstract.htm')
+  download.file('https://ndownloader.figshare.com/files/5593343',
+                'analysis/data/raw_data/quaternarymammals/quaternarymammals_data.txt')
+  download.file('https://ndownloader.figshare.com/files/5593346',
+                'analysis/data/raw_data/quaternarymammals/quaternarymammals_metadata.htm')
+
+  return(NULL)
+}
+
+
+#' Load the Body Mass of Quaternary Mammals databasee
+#'
+#' Loads Body Mass of Late Quaternary Mammals
+#'
+#' @return list of 4 data tables: `species`, `sites`, `communities`, and `trapping`
+#'
+#' @export
+load_quaternarymammals <- function() {
+  species <- read.csv(paste0( 'analysis/data/raw_data/mcdb/MCDB_species.csv'), stringsAsFactors = F)
+  sites <- read.csv(paste0( 'analysis/data/raw_data/mcdb/MCDB_sites.csv'), stringsAsFactors = F)
+  communities <- read.csv(paste0( 'analysis/data/raw_data/mcdb/MCDB_communities.csv'), stringsAsFactors = F)
+  trapping <- read.csv(paste0( 'analysis/data/raw_data/mcdb/MCDB_trapping.csv'), stringsAsFactors = F)
+
+  mcdb <- list(species, sites, communities, trapping)
+  names(mcdb) <- c('species', 'sites', 'communities', 'trapping')
+  return(mcdb)
+
+}
