ec15af7499845e751c5c68611397eae2ea5fcddd
Renata Diaz <diaz.renatam@gmail.com>
2019-02-25 15:27:44 -0500

    include code to download vertnet

--- use_retriever.R
+++ use_retriever.R
@@ -4,6 +4,7 @@ check_default_data_path()
 
 install_retriever_data("mammal-community-db")
 install_retriever_data("mammal-masses")
+install_retriever_data("vertnet-mammals")
 
 
 mcdb <- import_retriever_data("mammal-community-db")
